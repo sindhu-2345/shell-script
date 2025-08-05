d5033f546b0eefd7a9cbf5137ffd0d6a12be1a1c
sindhu-2345 <sammetasindhu2@gmail.com>
2025-08-05 21:03:09 +0530

    variables are using

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,4 +6,4 @@ PERSON2=suresh
 echo "$PERSON1:: Hai $PERSON2"
 echo "$PERSON2:: hai $PERSON1"
 echo "$PERSON1:: what are doing $PERSON2"
-eccho "$PERSON2:: good what about you $PERSON1"
+echo "$PERSON2:: good what about you $PERSON1"
