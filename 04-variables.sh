45198be8b11b5a2b1487746c8785f727e0d846e7
sindhu-2345 <sammetasindhu2@gmail.com>
2025-08-05 21:08:19 +0530

    arguments are added

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+   
+   PERSON1=$1
+   PERSON2=$2
+   
+echo "$PERSON1:: Hai $PERSON2"
+echo "$PERSON2:: hai $PERSON1"
+echo "$PERSON1:: what are doing $PERSON2"
+echo "$PERSON2:: good what about you $PERSON1"
