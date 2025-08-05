495b41d95daa0bfa448d293b121a04264122efd5
sindhu-2345 <sammetasindhu2@gmail.com>
2025-08-05 21:43:59 +0530

    arrays are using

---
+++ 07-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+NAMES=("sindhu" "sammeta" "sasi")
+
+echo " first name : ${NAMES[0]} "
+echo " second name : ${NAMES[1]} "
+echo " third name : ${NAMES[2]} "
+echo "all names are : ${NAMES[@]}
