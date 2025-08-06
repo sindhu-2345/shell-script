ef438d1f65385a680937cc2b9ad0936da1756b4d
sindhu-2345 <sammetasindhu2@gmail.com>
2025-08-06 12:11:42 +0530

    installing servers

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -4,9 +4,13 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
 echo "ERROR:: u must have sudo access to execute this script"
-
 exit 1
 fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+echo " not inn list mysql"
 dnf install mysql -y
 if [$? -ne 0]
 then 
@@ -15,7 +19,14 @@ exit 1
 else
 echo "installing MYSQL ..... success"
 fi
+else
+echo " already installed"
+fi
 
+dnf list installed git
+if [ $? -ne 0 ]
+then
+echo " not in list "
 dnf instal git -y
 if [$? -ne 0]
 then 
@@ -23,4 +34,7 @@ echo "installing git .... failure"
 exit 1
 else
 echo "installing git ..... success"
+fi
+else 
+echo "already installed"
 fi
