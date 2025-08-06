6bd18b2d2d12177299654eacdbe237641a1655ef
sindhu-2345 <sammetasindhu2@gmail.com>
2025-08-06 11:34:20 +0530

    conditions are using

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -3,8 +3,8 @@
 NUMBER1=$1
 if [$NUMBER -gt 100]
 then
- echo ("print given number is greater than 100")
+ echo "(print given number is greater than 100)"
 
  else
-   echo (given number is less and equal to 100)
+   echo "(given number is less and equal to 100)"
 fi   
